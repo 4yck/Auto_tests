1e2d9f330858faac68eb5cfaf6bb83eadaedb9bb
Anton Mamchuk <amamchuk@universe.dart.spb>
2016-10-17 17:33:06 +0300

    14th commit

--- trunk/src/main/java/com/dataart/selenium/framework/BaseTest.java
+++ trunk/src/main/java/com/dataart/selenium/framework/BaseTest.java
@@ -10,29 +10,43 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
 import static com.dataart.selenium.framework.BasePage.driver;
 
 public class BaseTest {
     private static Settings settings = new Settings();
 
     @BeforeSuite(alwaysRun = true)
-    public static void beforeSuite() throws InterruptedException {
+    public static void beforeSuite() throws IOException {
         //For running in Google Chrome
         //System.setProperty("webdriver.chrome.driver", "/Users/macchook/Documents/Drivers/chromedriver");
 
+        //Basic Authentification point 2
         /*FirefoxProfile profile = new FirefoxProfile();
         profile.setPreference("network.http.phishy-userpass-length", 255);
         driver = new FirefoxDriver(profile);
         driver.get("http://admin:admin@localhost:8080");*/
 
-        BasePage.driver = settings.getDriver();
+        /*BasePage.driver = settings.getDriver();
         BasePage.settings = settings;
         BasePage.driver.get(settings.getBaseUrl());
         if (!settings.getBrowser().equals(BrowserType.HTMLUNIT))
-            driver.manage().window().maximize();
-        //Thread.sleep(5000);
-        //Alert alert = driver.switchTo().alert();
-        //driver.switchTo().alert().authenticateUsing((Credentials) new UsernamePasswordCredentials("admin", "admin"));
+            driver.manage().window().maximize();*/
+
+        BasePage.driver = settings.getDriver();
+        BasePage.settings = settings;
+
+        driver.manage().window().maximize();
+
+        Runtime.getRuntime().exec("C:\\Users\\amamchuk\\Desktop\\AutoITScripts\\HandleAuthentification.exe");
+
+        BasePage.driver.get(settings.getBaseUrl());
+        if (!settings.getBrowser().equals(BrowserType.HTMLUNIT));
+
     }
 
     @AfterSuite(alwaysRun = true)
